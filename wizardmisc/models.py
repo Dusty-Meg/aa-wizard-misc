822018146fed4f22ff81774cbb53ad8901d4aad2
Matthew Moore <matthew.moore@answerdigital.com>
2023-12-22 09:23:53 +0000

    Add DustyBotCommands table for Discord DustyBot

--- wizardmisc/models.py
+++ wizardmisc/models.py
@@ -10,3 +10,8 @@ from django.db import models
 class Settings(models.Model):
     setting_id = models.CharField(max_length=500)
     value = models.CharField(max_length=500)
+
+
+class DustyBotCommands(models.Model):
+    key = models.TextField()
+    message = models.TextField()
