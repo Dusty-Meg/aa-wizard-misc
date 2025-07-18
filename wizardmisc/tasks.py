b6c30d24f73b43fba44f8c94d0f37286836ed8a9
Dusty Meg <matthewemmoore@googlemail.com>
2025-07-18 12:28:57 +0000

    Added task that will create timers for unanchoring structures

--- wizardmisc/tasks.py
+++ wizardmisc/tasks.py
@@ -3,6 +3,7 @@
 # Standard Library
 import json
 import logging
+import datetime
 
 # Third Party
 import requests
@@ -14,6 +15,10 @@ from django.conf import settings
 # Alliance Auth
 from allianceauth.authentication.models import UserProfile
 from allianceauth.eveonline.models import EveCharacter
+
+from structures.models import Notification as StructuresNotification
+from structures.models import Structure as StructuresStructure
+from structuretimers.models import Timer as StructureTimersTimer
 
 from .app_settings import HR_FORUM_WEBHOOK, JABBERBOT_URL
 from .models import Settings
@@ -94,3 +99,66 @@ def new_account_discord_ping():
     last_id.value = last_id_id
 
     last_id.save()
+
+
+@shared_task
+def structures_notification_unanchoring():
+    settings_key = "StructuresNotificationUnanchoringLastId"
+    last_id, created = Settings.objects.get_or_create(
+        setting_id=settings_key,
+        defaults={"setting_id": settings_key, "value": "1"},
+    )
+
+    last_id_id = int(last_id.value)
+
+    for notification in StructuresNotification.objects.filter(
+        id__gt=last_id_id, notif_type="StructureUnanchoring"
+    ):
+
+        parsed_text = notificaion.parsed_text()
+
+        if not parsed_text:
+            continue
+
+        if "ownerCorpName" in parsed_text:
+            owner_corp_name = parsed_text["ownerCorpName"]
+        if "solarsystemID" in parsed_text:
+            solar_system_id = parsed_text["solarsystemID"]
+        if "structureTypeID" in parsed_text:
+            structure_type_id = parsed_text["structureTypeID"]
+        if "timeLeft" in parsed_text:
+            time_left = parsed_text["timeLeft"]
+        if "ownerCorpLinkData" in parsed_text:
+            owner_corp_link_data = parsed_text["ownerCorpLinkData"][2]
+
+        structure = StructuresStructure.objects.filter(
+            structure_id=notification.structure_id
+        ).first()
+
+
+        timer = StructureTimersTimer.objects.create(
+            timer_type="Unanchoring",
+            location_details="",
+            structure_name=structure.name if structure else "Unknown Structure",
+            objective="FR",
+            date=time_left,
+            is_important=False,
+            owner_name=owner_corp_name,
+            is_opsec=False,
+            visibility="UN",
+            details_notes=f"Automatically created from Structures Notification for {notification.structure_name} at {datetime.datetime.now(timezone.utc).isoformat()}",
+            eve_alliance_id=1,
+            eve_corporation_id=370,
+            eve_solar_system_id=solar_system_id,
+            structure_type_id=structure_type_id,
+            last_updated_at=datetime.datetime.now(timezone.utc).isoformat(),
+        )
+
+        last_id_id = notification.id
+
+    last_id.value = last_id_id
+    last_id.save()
+
+
+            
+
