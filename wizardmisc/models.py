26ee2239fbf3069dc5b668ba831c1cce08094828
Matthew Moore <matthew.moore@answerdigital.com>
2025-07-21 22:38:34 +0100

    Add DustyBotTimers model to manage timer data for DustyBot

--- wizardmisc/models.py
+++ wizardmisc/models.py
@@ -15,3 +15,11 @@ class Settings(models.Model):
 class DustyBotCommands(models.Model):
     key = models.TextField()
     message = models.TextField()
+
+
+class DustyBotTimers(models.Model):
+    TimerId = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")
+    SystemName = models.TextField()
+    BeltType = models.TextField()
+    RespawnTime = models.DateTimeField(blank=False, default=None, null=False)
+    ReportedTime = models.DateTimeField(blank=False, default=None, null=False)
